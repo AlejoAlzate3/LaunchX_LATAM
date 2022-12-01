3496b393bc0d3b7214cafcc8d81883fa2541543c
Alejandro Alzate Tovar <78713566+AlejoAlzate3@users.noreply.github.com>
2022-11-30 22:36:00 -0500

    Para despues actualizar

--- Backend/NodeJS/index.js
+++
@@ -1,8 +0,0 @@
-//Proceso Asincrono de NodeJS
-http = require('http');
-
-console.log("1. Hola");
-http.get('http://httpstat.us/200', (resp) => {
-    console.log(`2. La api devolvio:  ${resp.statusCode}`);
-});
-console.log("3. Adios");
